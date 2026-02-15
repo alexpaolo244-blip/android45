45cccac72110e7caa7f750a2bb15ee548b75f740
alexpaolo244-blip <alexpaolo244@gmail.com>
2026-02-15 21:11:26 +0000

    Update MainActivity.java

--- app/src/main/java/com/shofyou/app/MainActivity.java
+++ app/src/main/java/com/shofyou/app/MainActivity.java
@@ -1,18 +1,11 @@
 package com.shofyou.app;
 
-import android.Manifest;
 import android.annotation.SuppressLint;
 import android.app.AlertDialog;
 import android.content.Intent;
-import android.content.pm.PackageManager;
-import android.graphics.Bitmap;
+import android.graphics.Color;
 import android.net.Uri;
 import android.os.Bundle;
-import android.os.Message;
-import android.provider.MediaStore;
-import android.view.View;
-import android.view.WindowInsets;
-import android.view.WindowInsetsController;
 import android.webkit.CookieManager;
 import android.webkit.ValueCallback;
 import android.webkit.WebChromeClient;
@@ -23,27 +16,23 @@ import android.webkit.WebViewClient;
 
 import androidx.activity.OnBackPressedCallback;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.content.FileProvider;
-
-import java.io.File;
-import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
     private WebView webView;
     private ValueCallback<Uri[]> fileCallback;
-    private String cameraPath;
 
-    private static final String HOME_URL = "https://shofyou.com";
+    private final String HOME_URL = "https://shofyou.com";
 
     @SuppressLint("SetJavaScriptEnabled")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
 
         setContentView(R.layout.activity_main);
 
-        hideSystemUI();
+        getWindow().setStatusBarColor(Color.TRANSPARENT);
 
         webView = findViewById(R.id.webview);
 
@@ -53,16 +42,16 @@ public class MainActivity extends AppCompatActivity {
         ws.setDomStorageEnabled(true);
         ws.setAllowFileAccess(true);
         ws.setAllowContentAccess(true);
-        ws.setMediaPlaybackRequiresUserGesture(false);
         ws.setUseWideViewPort(true);
         ws.setLoadWithOverviewMode(true);
+        ws.setMediaPlaybackRequiresUserGesture(false);
 
         CookieManager.getInstance().setAcceptCookie(true);
         CookieManager.getInstance().setAcceptThirdPartyCookies(webView, true);
 
-        webView.setWebViewClient(new MyWebViewClient());
+        webView.setWebViewClient(new Browser());
 
-        webView.setWebChromeClient(new MyChrome());
+        webView.setWebChromeClient(new Chrome());
 
         if(savedInstanceState != null)
             webView.restoreState(savedInstanceState);
@@ -72,28 +61,7 @@ public class MainActivity extends AppCompatActivity {
         handleBack();
     }
 
-    private void hideSystemUI() {
-
-        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.R) {
-
-            getWindow().setDecorFitsSystemWindows(false);
-
-            WindowInsetsController controller =
-                    getWindow().getInsetsController();
-
-            if(controller != null) {
-
-                controller.hide(WindowInsets.Type.statusBars()
-                        | WindowInsets.Type.navigationBars());
-
-                controller.setSystemBarsBehavior(
-                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
-            }
-
-        }
-    }
-
-    private class MyWebViewClient extends WebViewClient {
+    private class Browser extends WebViewClient {
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view,
@@ -101,8 +69,11 @@ public class MainActivity extends AppCompatActivity {
 
             String url = request.getUrl().toString();
 
-            if(url.contains("shofyou.com"))
-                return false;
+            if(url.contains("shofyou.com")) {
+
+                view.loadUrl(url);
+                return true;
+            }
 
             Intent intent =
                     new Intent(MainActivity.this,
@@ -116,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    private class MyChrome extends WebChromeClient {
+    private class Chrome extends WebChromeClient {
 
         @Override
         public boolean onShowFileChooser(WebView webView,
@@ -125,73 +96,16 @@ public class MainActivity extends AppCompatActivity {
 
             fileCallback = callback;
 
-            Intent cameraIntent =
-                    new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            Intent intent =
+                    new Intent(Intent.ACTION_PICK);
 
-            File photoFile = null;
+            intent.setType("image/* video/*");
 
-            try {
+            intent.putExtra(Intent.EXTRA_ALLOW_MULTIPLE, true);
 
-                photoFile = File.createTempFile(
-                        "camera",
-                        ".jpg",
-                        getExternalFilesDir(null));
-
-                cameraPath = photoFile.getAbsolutePath();
-
-                Uri uri = FileProvider.getUriForFile(
-                        MainActivity.this,
-                        getPackageName() + ".provider",
-                        photoFile);
-
-                cameraIntent.putExtra(
-                        MediaStore.EXTRA_OUTPUT, uri);
-
-            } catch (IOException e) {}
-
-            Intent fileIntent =
-                    new Intent(Intent.ACTION_GET_CONTENT);
-
-            fileIntent.setType("*/*");
-
-            fileIntent.putExtra(
-                    Intent.EXTRA_ALLOW_MULTIPLE, true);
-
-            Intent chooser =
-                    new Intent(Intent.ACTION_CHOOSER);
-
-            chooser.putExtra(
-                    Intent.EXTRA_INTENT, fileIntent);
-
-            chooser.putExtra(
-                    Intent.EXTRA_INITIAL_INTENTS,
-                    new Intent[]{cameraIntent});
-
-            startActivityForResult(chooser, 100);
+            startActivityForResult(intent, 100);
 
             return true;
-        }
-
-        @Override
-        public boolean onCreateWindow(WebView view,
-                                      boolean dialog,
-                                      boolean userGesture,
-                                      Message resultMsg) {
-
-            WebView.HitTestResult result =
-                    view.getHitTestResult();
-
-            String url = result.getExtra();
-
-            Intent intent =
-                    new Intent(MainActivity.this,
-                            PopupActivity.class);
-
-            intent.putExtra("url", url);
-
-            startActivity(intent);
-
-            return false;
         }
     }
 
@@ -200,30 +114,16 @@ public class MainActivity extends AppCompatActivity {
                                     int resultCode,
                                     Intent data) {
 
-        if(fileCallback == null)
-            return;
+        if(fileCallback == null) return;
 
-        Uri[] results = null;
+        Uri[] result = null;
 
-        if(resultCode == RESULT_OK) {
+        if(resultCode == RESULT_OK && data != null) {
 
-            if(data == null) {
-
-                File file =
-                        new File(cameraPath);
-
-                results =
-                        new Uri[]{Uri.fromFile(file)};
-            }
-
-            else {
-
-                results =
-                        new Uri[]{data.getData()};
-            }
+            result = new Uri[]{data.getData()};
         }
 
-        fileCallback.onReceiveValue(results);
+        fileCallback.onReceiveValue(result);
 
         fileCallback = null;
     }
@@ -243,8 +143,7 @@ public class MainActivity extends AppCompatActivity {
                 else {
 
                     new AlertDialog.Builder(MainActivity.this)
-                            .setTitle("Exit")
-                            .setMessage("Do you want exit?")
+                            .setMessage("Exit app?")
                             .setPositiveButton("Yes",
                                     (d,i)->finish())
                             .setNegativeButton("No", null)
@@ -262,4 +161,17 @@ public class MainActivity extends AppCompatActivity {
         super.onSaveInstanceState(outState);
     }
 
+    @Override
+    protected void onPause() {
+
+        webView.onPause();
+        super.onPause();
+    }
+
+    @Override
+    protected void onResume() {
+
+        webView.onResume();
+        super.onResume();
+    }
 }
